6adc7d9c0378434ceaec9c0a5cd5cffe81a3e0c9
pamentoro <pamentoro88@gmail.com>
2021-06-20 17:22:44 +0100

    Back to master

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
 
 const Banner = () => {
-  return <></>;
+  const faAngle = <FontAwesomeIcon icon={faAngleDoubleRight} />;
+  return (
+    <div className="banner-container">
+      <h1 className="banner-title">I'm Kortino</h1>
+      <div className="banner-jobs">
+        <h2>
+          Developer
+          <span>{faAngle}</span>
+        </h2>
+        <h2>
+          Designer
+          <span>{faAngle}</span>
+        </h2>
+        <h2>
+          Photographer
+          <span>{faAngle}</span>
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,7 +1,59 @@
-import React from "react";
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faBars } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  return <></>;
+  const [display, setDisplay] = useState(false);
+  const linksHandler = () => {
+    setDisplay(!display);
+    console.log(display);
+  };
+
+  const logo = <FontAwesomeIcon icon={faPlay} />;
+  const hamburger = <FontAwesomeIcon icon={faBars} />;
+  return (
+    <div className="nav-container">
+      <div className="nav-header">
+        <div className="nav-logo">{logo}</div>
+        <div className="nav-btn" onClick={linksHandler}>
+          {hamburger}
+        </div>
+      </div>
+      <ul className={display ? "show-links" : "nav-links"}>
+        <li>
+          <a href="#" className="nav-link">
+            Home{display}
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link">
+            Services
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link">
+            Numbers
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link">
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#" className="nav-link">
+            Contact
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
